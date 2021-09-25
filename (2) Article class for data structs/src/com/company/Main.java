1c6be10e5a7b3117ccce2ebeac81079eedce1f6b
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2021-09-25 15:35:58 +0300

    feat: add mappings for maps e.g. author-number of articles

--- (2) Article class for data structs/src/com/company/Main.java
+++ (2) Article class for data structs/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayDeque;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -17,34 +18,83 @@ public class Main {
         q.offer(new Article("gg", "hh", "jj", 15, 1989));
         q.offer(new Article("vv", "bb", "nn", 17, 1945));
         q.offer(new Article("uu", "ii", "oo", 45, 1333));
+        q.offer(new Article("a", "b", "c", 5, 1986));
+        q.offer(new Article("tt", "yy", "uu", 26, 1955));
+        q.offer(new Article("gg", "hh", "jj", 15, 1989));
+        q.offer(new Article("vv", "bb", "nn", 17, 1945));
+        q.offer(new Article("vv", "bb", "nn", 18, 1945));
+
+
         for (Article item:q){
             System.out.println((item.toString()));
         }
 
         System.out.println("-----------------");
-        if (q.size() >= 5){
+
+        if (q.size() >= 10){
             q.poll();
             q.offer(new Article("oo", "ppi", "ll", 99, 1222));
         }
         else {
             q.offer(new Article("yy", "ttt", "ss", 44, 1000));
         }
+        for (Article item:q){
+            System.out.println((item.toString()));
+        }
+        System.out.println("-----------------");
 
-        HashMap <String, Integer> m = new HashMap<String, Integer>();
+        HashMap <String, Integer> countArticle = new HashMap<String, Integer>();
 
         for (Article item:q){
-            if (m.containsKey(item.autor)){
-                m.put(item.autor, m.get(item.autor) + 1);
+            if (countArticle.containsKey(item.autor)){
+                countArticle.put(item.autor, countArticle.get(item.autor) + 1);
             }
             else{
-                m.put(item.autor, 1);
+                countArticle.put(item.autor, 1);
             }
         }
-
         // перебор элементов
-        for(HashMap.Entry item : m.entrySet())
+        for(HashMap.Entry item : countArticle.entrySet())
         {
-            System.out.println("Key: "+item.getKey()+"; Value: "+item.getValue());
+            System.out.println("Autor: "+item.getKey()+"; Count articles: "+item.getValue());
+        }
+        System.out.println("+++++++++++++++");
+
+        HashMap <String, Integer> countViews = new HashMap<String, Integer>();
+        for (Article item:q){
+            if (countViews.containsKey(item.autor)){
+
+                countViews.put(item.autor, countViews.get(item.autor) + item.count);
+            }
+            else{
+                countViews.put(item.autor, item.count);
+            }
+        }
+        for(HashMap.Entry item : countViews.entrySet())
+        {
+            System.out.println("Autor: "+item.getKey()+"; Count Views: "+item.getValue());
+        }
+        System.out.println("=================");
+
+        HashMap <Integer, Integer> articlesByYear = new HashMap<Integer, Integer>();
+        for (Article item:q){
+            if (articlesByYear.containsKey(item.year)){
+                articlesByYear.put(item.year, articlesByYear.get(item.year) + item.count);
+            }
+            else{
+                articlesByYear.put(item.year, item.count);
+            }
+        }
+        for(HashMap.Entry item : articlesByYear.entrySet())
+        {
+            System.out.println("Year: "+item.getKey()+"; Count Articles: "+item.getValue());
+        }
+
+        System.out.println();
+        countViews = a.removeLess40(countViews);
+        for(HashMap.Entry item : countViews.entrySet())
+        {
+            System.out.println("Autor: "+item.getKey()+"; Count Views: "+item.getValue());
         }
     }
 }
@@ -66,5 +116,15 @@ class Article {
 
     public String toString() {
         return this.autor + " " + this.name + " " + this.count;
+    }
+
+    public HashMap <String, Integer> removeLess40(HashMap <String, Integer> countViews){
+        HashMap <String, Integer> countViews2 = new HashMap <String, Integer>(countViews);
+        for (HashMap.Entry item : countViews.entrySet()){
+            if ((int)item.getValue() < 40){
+                countViews2.remove(item.getKey());
+            }
+        }
+        return countViews2;
     }
 }
