72aea59f3e99664074756588917f592bfe28ef11
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2021-10-11 14:44:05 +0300

    feat: add a file system project

---
+++ FileText/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyForm f = new MyForm();
+        f.pack();
+        f.setVisible(true);
+    }
+}
