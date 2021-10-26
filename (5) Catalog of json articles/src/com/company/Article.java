2f3f325f8d103e4cb5d453e80395ae8a59aa8649
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2021-10-27 00:25:55 +0300

    feat: add grouping and output of unique keys

--- (5) Catalog of json articles/src/com/company/Article.java
+++ (5) Catalog of json articles/src/com/company/Article.java
@@ -1,27 +1,26 @@
 package com.company;
 
 public class Article {
-    public String author;
-    public String rubric;
     public String title;
+    public String author;
     public int year;
+    public String category;
     public String[] keyWord;
 
-    public Article()
-    {
+    public Article() {
         super();
     }
 
-    public Article(String author, String rubric, String name, int year, String[] keyWord) {
+    public Article(String name, String author, int year, String category, String[] keyWord) {
         this.author = author;
-        this.rubric = rubric;
+        this.category = category;
         this.title = name;
         this.year = year;
         this.keyWord = keyWord;
     }
 
-    public String toString() {
-        return this.author + " " + this.title + " " + this.year + " " + this.rubric;
-    }
+//    public String toString() {
+//        return this.author + " " + this.title + " " + this.year + " " + this.rubric;
+//    }
 
 }
